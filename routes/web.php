3fcaa33ebdbe2c13b9b59c5edd7abd916eb53d56
vantuan02 <vantuan334456@gmail.com>
2024-10-08 14:40:31 +0700

    thêm test index user

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserControler;
 
 Route::get('/', function(){
     echo 'abc';
  });
 
 route::get('list-product', [ProductController::class, 'show']);
+
 route::get('get-product/{id}', [ProductController::class, 'getProduct']);
+
 route::get('update-product', [ProductController::class, 'updateProduct']);
+
+Route::resource('users', UserControler::class);
