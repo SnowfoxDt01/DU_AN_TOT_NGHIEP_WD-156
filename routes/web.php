b911ea1728d961a591078a1f61b7dd7b9d14e79b
vantuan02 <vantuan334456@gmail.com>
2024-10-09 14:52:55 +0700

    Them giao dien admin va chuc nang,create user, logout

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,13 @@ Route::get('/', function(){
 
  Auth::routes();
 
+Route::resource('/users', UserControler::class);
+
 route::get('list-product', [ProductController::class, 'show']);
 
 route::get('get-product/{id}', [ProductController::class, 'getProduct']);
 
 route::get('update-product', [ProductController::class, 'updateProduct']);
-
-Route::resource('users', UserControler::class);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware('role:super-admin')->group(function () {
     Route::get('/role-permission', [RolePermissionController::class, 'index'])->name('role-permission.index');
