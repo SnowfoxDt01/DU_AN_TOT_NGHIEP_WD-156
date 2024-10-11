088f8037888c9a4953dc839d537c77c7bcd9ce38
vantuan02 <vantuan334456@gmail.com>
2024-10-11 11:15:00 +0700

    cap nhat status cho user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,6 +8,7 @@
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
                 <th scope="col">Password</th>
+                <th scope="col">Status</th>
                 <th scope="col">Action</th>
             </tr>
         </thead>
@@ -18,7 +19,18 @@
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->password }}</td>
-                    <td>{{ $user->role }}</td>
+                    <td>
+                        @if ($user->status == 0)
+                            <span class="badge bg-red">Dừng hoạt động</span>
+                        @elseif($user->status == 1)
+                            <span class="badge bg-green">Đang hoạt động</span>
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{ route('users.edit', $user->id) }}">
+                            <button class="btn btn-success"><i class="bi bi-pencil-square"></i></button>
+                        </a>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
