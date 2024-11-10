8e2ac4c4237bcaad2ecd224df968fabd65859e3c
Nhat Bloom <nhatlong2304@gmail.com>
2024-11-10 23:26:31 +0700

    edit profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,9 +1,8 @@
 <?php
 
-// app/Models/User.php
-
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -14,17 +13,32 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
@@ -35,18 +49,21 @@ class User extends Authenticatable
         return $this->hasMany(Review::class, 'user_id');
     }
 
-    public function customer()
+    public function customers()
     {
-        return $this->hasOne(Customer::class, 'id_user'); // Liên kết với bảng customers
+        return $this->hasMany(Customer::class, 'id_user');
     }
-
+    
     public function orders()
     {
         return $this->hasMany(ShopOrder::class, 'user_id');
     }
 
+
     public function shoppingCart()
     {
         return $this->hasOne(ShoppingCart::class, 'user_id'); 
     }
+
+
 }
