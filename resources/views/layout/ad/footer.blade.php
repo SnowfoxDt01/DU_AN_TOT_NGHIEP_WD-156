cd777426aa4e3865a9873ac1f0c92d8474960058
vantuan02 <vantuan334456@gmail.com>
2024-12-10 18:44:56 +0700

    base admin mới

---
+++ resources/views/layout/ad/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="py-6 px-6 text-center">
+    <p class="mb-0 fs-4"><a href=""
+            target="_blank" class="pe-1 text-primary text-decoration-underline">VSNK SHOP</a>
+    </p>
+</div>
